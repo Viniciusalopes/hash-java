c97211aa6a50c9d7b74a32c5692afdbc522a0fef
Viniciusalopes <suporte@viniciusalopes.com.br>
2021-01-23 14:11:20 -0300

    Licenseheader and build options

---
+++ HashJava/src/br/com/vovolinux/hashjava/Hash.java
@@ -0,0 +1,18 @@
+/*
+ *  License   : MIT - Copyright 2021 Viniciusalopes (Vovolinux) <suporte@vovolinux.com.br>
+ *  Author    : Vinicius Lopes
+ *  Date      : 23/01/2021 14:10:25 
+ *  Project   : HashJava
+ *  Version   : 0.0.1
+ *  Purpose   :
+ *  Changelog : 2020-01-00 v.0.0.2 (Vinicius Lopes)
+ */
+package br.com.vovolinux.hashjava;
+
+/**
+ *
+ * @author vovomint
+ */
+public class Hash {
+    
+}
